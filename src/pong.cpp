2665e26f6e3a9e9e27a5c008f414320c8a470c20
Aaron McKenney <aaronmckenney@gmail.com>
2014-06-28 15:41:11 -0700

    Made key variables have global status and moved game loops into their own functions.

--- src/pong.cpp
+++ src/pong.cpp
@@ -2,8 +2,6 @@
 #include <string>
 #include <sstream>
 
-//Operating Systems headers
-
 //SDL headers
 #include "SDL/SDL.h"
 #include "SDL/SDL_image.h"
@@ -15,6 +13,30 @@
 #include "../include/paddle.h"
 #include "../include/ball.h"
 
+//namespaces
+using namespace std;
+
+//The paddles
+Paddle pad1 = Paddle(P1);
+Paddle pad2 = Paddle(P2);
+
+//The ball
+Ball ball = Ball(0);
+
+//Used for regulating the frame rate
+Timer fps = Timer();
+
+//The event structure that stores event data
+SDL_Event event;
+
+//If true, exit the game
+bool quit = false;
+
+//The players score, and the winner of the game.
+int p1_score = 0;
+int p2_score = 0;
+int winner = -1;
+
 //The surface (an extern variable in board.h)
 SDL_Surface *screen = NULL;
 
@@ -25,16 +47,9 @@ SDL_Surface *p2_score_msg = NULL;
 //The surface for displaying the win message
 SDL_Surface *win_msg = NULL;
 
-//The event structure that stores event data
-SDL_Event event;
-
 //The font and font color for the score messages
 TTF_Font *font = NULL;
 SDL_Color fontColor = {0xFF, 0xFF, 0xFF};
-
-SDL_Surface *loadImage(std::string filename);
-
-using namespace std;
 
 //initializes SDL functionality
 bool SDLInit()
@@ -108,7 +123,7 @@ void ShowTopPanel(int p1_score, int p2_score)
     p2_score_str << p2_score;
     p2_score_msg = TTF_RenderText_Solid(font, p2_score_str.str().c_str(), 
                                         fontColor);
-    applySurface(SCREEN_WIDTH - (p2_score_msg->clip_rect.w + SCORE_MSG_X_OFFSET), 
+    applySurface(SCREEN_WIDTH - (p2_score_msg->clip_rect.w+SCORE_MSG_X_OFFSET),
                  SCORE_MSG_Y_OFFSET, p2_score_msg, screen);
 }
 
@@ -121,7 +136,8 @@ void ShowMidLine()
     mid_line.w = 1;
     mid_line.h = SCREEN_HEIGHT;
 
-    SDL_FillRect(screen, &mid_line, SDL_MapRGB(screen->format, 0xFF, 0xFF, 0xFF));
+    SDL_FillRect(screen, &mid_line, 
+                 SDL_MapRGB(screen->format, 0xFF, 0xFF, 0xFF));
 }
 
 //Show the bottom panel
@@ -133,10 +149,11 @@ void ShowBotPanel()
     bot_line.w = SCREEN_WIDTH;
     bot_line.h = BOT_PANEL_LINE_HEIGHT;
 
-    SDL_FillRect(screen, &bot_line, SDL_MapRGB(screen->format, 0xFF, 0xFF, 0xFF));
+    SDL_FillRect(screen, &bot_line, 
+                 SDL_MapRGB(screen->format, 0xFF, 0xFF, 0xFF));
 }
 
-//Sees if anyone has scored a point, sets scores accordingly.
+//Sees if anyone has scored a point and sets scores accordingly.
 //returns true if someone scored a point, and false otherwise.
 bool updateScores(SDL_Rect box, int *p1_score, int *p2_score)
 {
@@ -187,35 +204,27 @@ void showWinMsg(int winner)
     applySurface(win_msg->clip_rect.x, win_msg->clip_rect.y, win_msg, screen);
 }
 
-int main()
+//Show the board
+void showBoard()
 {
-    bool quit = false;
+    //First blackout the screen to remove previous information
+    SDL_FillRect(screen, &screen->clip_rect, 
+                 SDL_MapRGB(screen->format, 0, 0, 0));
 
-    int p1_score = 0;
-    int p2_score = 0;
-    int winner = -1;
+    //Display the panels that exist outside the gaming area.
+    ShowTopPanel(p1_score, p2_score);
+    ShowMidLine();
+    ShowBotPanel();
 
-    //Seed the random number generator
-    srand(time(NULL));
+    //Display our objects
+    pad1.show();
+    pad2.show();
+    ball.show();
+}
 
-    //The paddles
-    Paddle pad1 = Paddle(P1);
-    Paddle pad2 = Paddle(P2);
-
-    //The ball
-    Ball ball = Ball(0);
-    //(ix, iy) is the point of impact that the ball has had with an object
-    //float ix = 0, iy = 0;
-    //What the ball has collided with during the time step
-    //int ball_coll;
-
-    //Used for regulating the frame rate
-    Timer fps = Timer();
-
-    if(!SDLInit())
-        return 1;
-
-    //GAME LOOP
+//Pong is played here
+void gameLoop()
+{
     while(!quit)
     {
         //Start the frame rate regulator
@@ -241,36 +250,22 @@ int main()
         //Handle any collisions that the paddles could have
         pad1.handleCollisions();
         pad2.handleCollisions();
-
         //Detect and handle any collisions that the ball has.
-        /*
-        ball_coll = ball.detectCollision(pad1.getBox(), pad2.getBox(), &ix, &iy);
-        if(ball_coll != HIT_NOTHING && ball_coll != HIT_ERROR)
-            ball.handleCollisions(pad1.getBox(), pad2.getBox(), ball_coll, ix, iy);
-        */
         ball.handleCollisions(pad1, pad2);
         
+        //Update the player's scores.
+        //If they had to be updated, make a new ball.
         if(updateScores(ball.getBox(), &p1_score, &p2_score))
             ball = Ball(p1_score + p2_score);
 
-        //First blackout the screen to remove previous information
-        SDL_FillRect(screen, &screen->clip_rect, 
-                     SDL_MapRGB(screen->format, 0, 0, 0));
-        //Display the panels that exist outside the gaming area.
-        ShowTopPanel(p1_score, p2_score);
-        ShowMidLine();
-        ShowBotPanel();
-        //Display our objects
-        pad1.show();
-        pad2.show();
-        ball.show();
+        //Show the board
+        showBoard();
 
-        //Update the screen
+        //Update the screen. SDL docs don't say how SDL_Flip can fail, only 
+        //that it can. I don't think that it's worthy of shutting down the 
+        //game if it does fail.
         if(SDL_Flip(screen) == -1)
-        {
             fprintf(stderr, "Failed to update the screen\n");
-            return 1;
-        }
 
         //Cap the frame rate at 1000/MAX_FRAMES_PER_SEC milliseconds
         if(fps.getTicks() < 1000*DT)
@@ -283,45 +278,43 @@ int main()
         if(winner == P1 || winner == P2)
             break;
     }
+}
+
+//If someone wins the game, loop here to display the win message.
+void gameOverLoop()
+{
+    while(!quit)
+    {
+        //Handle Events
+        while(SDL_PollEvent(&event))
+        {
+            //Quit if the user closes the window
+            if(event.type == SDL_QUIT)
+                quit = true;
+        }
+
+        //Show the board
+        showBoard();
+        //Show the win message on top of the board
+        showWinMsg(winner);
+
+        //Update the screen. SDL docs don't say how SDL_Flip can fail, only 
+        //that it can. I don't think that it's worthy of shutting down the 
+        //game if it does fail.
+        if(SDL_Flip(screen) == -1)
+            fprintf(stderr, "Failed to update the screen\n");
+    }
+}
+
+int main()
+{
+    if(!SDLInit())
+        return 1;
+
+    gameLoop();
 
     if(winner == P1 || winner == P2)
-    {
-        //GAMEOVER LOOP
-        while(!quit)
-        {
-            //Handle Events
-            while(SDL_PollEvent(&event))
-            {
-                //Adjust the paddles' velocity here
-                pad1.handleInput(event);
-                pad2.handleInput(event);
-    
-                //Quit if the user closes the window
-                if(event.type == SDL_QUIT)
-                    quit = true;
-            }
-
-            //Show who won and whatnot
-            //First blackout the screen to remove previous information
-            SDL_FillRect(screen, &screen->clip_rect, 
-                         SDL_MapRGB(screen->format, 0, 0, 0));
-            //Display the panels that exist outside the gaming area.
-            ShowTopPanel(p1_score, p2_score);
-            ShowMidLine();
-            ShowBotPanel();
-            pad1.show();
-            pad2.show();
-
-            showWinMsg(winner);
-
-            //Update the screen
-            if(SDL_Flip(screen) == -1)
-            {
-                fprintf(stderr, "Failed to update the screen\n");
-                return 1;
-            }
-        }
-    }
+        gameOverLoop();
 
     SDLQuit();
     return 0;
